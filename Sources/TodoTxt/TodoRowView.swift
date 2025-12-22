16f2fd60105e32eab0c5d7100f98c71a8550edc4
moritzmhmk <moritzmhmk@gmail.com>
2025-12-22 09:20:50 +0100

    Render tokens.

--- Sources/TodoTxt/TodoRowView.swift
+++ Sources/TodoTxt/TodoRowView.swift
@@ -13,9 +13,45 @@ struct TodoRowView: View {
             }
             .buttonStyle(.plain)
 
-            Text(item.description)
-                .strikethrough(item.completed)
+            tokensView(item.tokens)
         }
         .padding(.vertical, 4)
     }
+}
+
+func tokensView(_ tokens: [Token]) -> some View {
+    HStack(alignment: .firstTextBaseline, spacing: 6) {
+        ForEach(tokens, id: \.description) { token in
+            switch token {
+            case .context(let s):
+                Text("+\(s)").font(.system(size: 12, weight: .light)).foregroundColor(.secondary)
+
+            case .project(let s):
+                Text("@\(s)").font(.system(size: 12, weight: .light)).foregroundColor(.secondary)
+
+            case .keyValue(let key, let value):
+                keyValueView(key: key, value: value)
+
+            case .word(let s):
+                Text(s)
+            }
+        }
+    }
+}
+
+private func keyValueView(key: String, value: String) -> some View {
+    HStack(spacing: 1) {
+        Text("\(key)")
+            .padding(.horizontal, 5)
+            .padding(.vertical, 2)
+            .background(Color(nsColor: .tertiarySystemFill))
+
+        Text(value)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 2)
+            .background(Color(nsColor: .secondarySystemFill))
+    }
+    .font(.system(size: 12))
+    .foregroundColor(.secondary)
+    .clipShape(RoundedRectangle(cornerRadius: 4))
 }
